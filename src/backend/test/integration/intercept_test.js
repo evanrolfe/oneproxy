cf04ea3eeb716d1d3eede00e9ca02b159d8d2522
Evan Rolfe <evanrolfe@gmail.com>
2020-06-30 10:33:34 +0100

    Fix intercept tests not working on circleCI

--- src/backend/test/integration/intercept_test.js
+++ src/backend/test/integration/intercept_test.js
@@ -91,7 +91,7 @@ describe('The Intercept', () => {
       expect(request.http_version).to.eql('1.1');
       expect(request.path).to.eql('/');
       expect(request.ext).to.eql(null);
-      expect(request.request_headers).to.eql('{"host":"localhost:3000","user-agent":"curl/7.58.0","accept":"*/*"}');
+      expect(request.request_headers).to.include('curl');
 
       // Modified Request:
       expect(request.modified_url).to.eql(null);
@@ -126,102 +126,102 @@ describe('The Intercept', () => {
     });
   });
 
-//   describe('Intercepting a GET request', () => {
-//     it('works', async () => {
-//       const curlRequestFinished = makeRequest(proxyPort);
-//       const message = await messageFromBackend('requestIntercepted');
+  describe('Intercepting a GET request', () => {
+    it('works', async () => {
+      const curlRequestFinished = makeRequest(proxyPort);
+      const message = await messageFromBackend('requestIntercepted');
 
-//       console.log(`[TEST] sending forward to backend...`)
-//       writeToBackend({
-//         "command": "forward",
-//         "request": {
-//           "id": message.request.id,
-//           "rawRequest": 'GET /api/posts.json HTTP/1.1\nhost: localhost:3000\nuser-agent: curl/1.2.3\naccept: */*\n'
-//         }
-//       });
-//       const response = await curlRequestFinished;
+      console.log(`[TEST] sending forward to backend...`)
+      writeToBackend({
+        "command": "forward",
+        "request": {
+          "id": message.request.id,
+          "rawRequest": 'GET /api/posts.json HTTP/1.1\nhost: localhost:3000\nuser-agent: curl/1.2.3\naccept: */*\n'
+        }
+      });
+      const response = await curlRequestFinished;
 
-//       const result = await global.knex('requests').where({ id: message.request.id });
-//       const request = result[0];
-//       console.log(request)
+      const result = await global.knex('requests').where({ id: message.request.id });
+      const request = result[0];
+      console.log(request)
 
-//       // Original Request:
-//       expect(request.url).to.eql('http://localhost:3000/');
-//       expect(request.method).to.eql('GET');
-//       expect(request.host).to.eql('localhost');
-//       expect(request.port).to.eql(3000);
-//       expect(request.http_version).to.eql('1.1');
-//       expect(request.path).to.eql('/');
-//       expect(request.ext).to.eql(null);
-//       expect(request.request_headers).to.eql('{"host":"localhost:3000","user-agent":"curl/7.58.0","accept":"*/*"}');
+      // Original Request:
+      expect(request.url).to.eql('http://localhost:3000/');
+      expect(request.method).to.eql('GET');
+      expect(request.host).to.eql('localhost');
+      expect(request.port).to.eql(3000);
+      expect(request.http_version).to.eql('1.1');
+      expect(request.path).to.eql('/');
+      expect(request.ext).to.eql(null);
+      expect(request.request_headers).to.include('curl');
 
-//       // Modified Request:
-//       expect(request.modified_url).to.eql('http://localhost:3000/api/posts.json');
-//       expect(request.modified_method).to.eql('GET');
-//       expect(request.modified_host).to.eql('localhost');
-//       expect(request.modified_port).to.eql(3000);
-//       expect(request.modified_http_version).to.eql('1.1');
-//       expect(request.modified_path).to.eql('/api/posts.json');
-//       expect(request.modified_ext).to.eql('json');
-//       expect(request.modified_request_headers).to.eql('{"host":"localhost:3000","user-agent":"curl/1.2.3","accept":"*/*"}');
+      // Modified Request:
+      expect(request.modified_url).to.eql('http://localhost:3000/api/posts.json');
+      expect(request.modified_method).to.eql('GET');
+      expect(request.modified_host).to.eql('localhost');
+      expect(request.modified_port).to.eql(3000);
+      expect(request.modified_http_version).to.eql('1.1');
+      expect(request.modified_path).to.eql('/api/posts.json');
+      expect(request.modified_ext).to.eql('json');
+      expect(request.modified_request_headers).to.include('curl');
 
-//       // Response:
-//       expect(request.response_status).to.eql(200);
-//       expect(request.response_status_message).to.eql('OK');
-//       expect(request.response_http_version).to.eql('1.1');
-//       expect(request.response_headers).to.include('application/json');
-//       expect(request.response_body).to.include('[{"id":1,');
+      // Response:
+      expect(request.response_status).to.eql(200);
+      expect(request.response_status_message).to.eql('OK');
+      expect(request.response_http_version).to.eql('1.1');
+      expect(request.response_headers).to.include('application/json');
+      expect(request.response_body).to.include('[{"id":1,');
 
-//       // General:
-//       expect(request.client_id).to.eql(1);
-//       expect(request.request_modified).to.eql(1);
-//       expect(request.response_modified).to.eql(0);
-//     });
-//   });
+      // General:
+      expect(request.client_id).to.eql(1);
+      expect(request.request_modified).to.eql(1);
+      expect(request.response_modified).to.eql(0);
+    });
+  });
 
-//   describe('Intercepting a POST request', () => {
-//     it('works', async () => {
-//       const curlCommand = `curl --request POST http://localhost:3000/api/posts.json --header 'Content-Type: application/json' --data-raw '{"name": "intercept_test"}'  --proxy http://127.0.0.1:${proxyPort}`
-//       const curlRequestFinished = makeRequest(proxyPort, curlCommand);
-//       const message = await messageFromBackend('requestIntercepted');
+  describe('Intercepting a POST request', () => {
+    it('works', async () => {
+      const curlCommand = `curl --request POST http://localhost:3000/api/posts.json --header 'Content-Type: application/json' --data-raw '{"name": "intercept_test"}'  --proxy http://127.0.0.1:${proxyPort}`
+      const curlRequestFinished = makeRequest(proxyPort, curlCommand);
+      const message = await messageFromBackend('requestIntercepted');
 
-//       console.log(`[TEST] sending forward to backend...`)
+      console.log(`[TEST] sending forward to backend...`)
 
-//       const rawRequest = `POST /api/posts.json HTTP/1.1
-// host: localhost:3000
-// user-agent: curl/7.58.0
-// accept: */*
-// content-type: application/json
-// content-length: 26
+      const rawRequest = `POST /api/posts.json HTTP/1.1
+host: localhost:3000
+user-agent: curl/7.58.0
+accept: */*
+content-type: application/json
+content-length: 26
 
-// {"name": "evan"}`;
+{"name": "evan"}`;
 
-//       writeToBackend({
-//         "command": "forward",
-//         "request": {
-//           "id": message.request.id,
-//           "rawRequest": rawRequest
-//         }
-//       });
-//       const response = await curlRequestFinished;
+      writeToBackend({
+        "command": "forward",
+        "request": {
+          "id": message.request.id,
+          "rawRequest": rawRequest
+        }
+      });
+      const response = await curlRequestFinished;
 
-//       const result = await global.knex('requests').where({ id: message.request.id });
-//       const request = result[0];
-//       console.log(request)
+      const result = await global.knex('requests').where({ id: message.request.id });
+      const request = result[0];
+      console.log(request)
 
-//       // Original Request:
-//       expect(request.url).to.eql('http://localhost:3000/api/posts.json');
-//       expect(request.method).to.eql('POST');
-//       expect(request.request_payload).to.eql('{"name": "intercept_test"}');
+      // Original Request:
+      expect(request.url).to.eql('http://localhost:3000/api/posts.json');
+      expect(request.method).to.eql('POST');
+      expect(request.request_payload).to.eql('{"name": "intercept_test"}');
 
-//       // Modified Request:
-//       expect(request.modified_url).to.eql('http://localhost:3000/api/posts.json');
-//       expect(request.modified_method).to.eql('POST');
-//       expect(request.modified_request_payload).to.eql('{"name": "evan"}');
+      // Modified Request:
+      expect(request.modified_url).to.eql('http://localhost:3000/api/posts.json');
+      expect(request.modified_method).to.eql('POST');
+      expect(request.modified_request_payload).to.eql('{"name": "evan"}');
 
-//       // Response:
-//       expect(response).to.eql('{"message":"Hello, you told us your name is: evan"}')
-//     });
-//   });
+      // Response:
+      expect(response).to.eql('{"message":"Hello, you told us your name is: evan"}')
+    });
+  });
 });
 
